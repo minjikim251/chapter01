f68a9b15e60c4ef60c7d3a8499d9473a4581a338
minjikim251 <minjikim25@gmail.com>
2020-05-11 21:17:51 +0900

    switch문 테스트

--- src/chapter01/SwitchTest.java
+++ src/chapter01/SwitchTest.java
@@ -40,17 +40,44 @@ public class SwitchTest {
 		
 		
 		
-		if( month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+//		if( month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+//			days = 31;
+//		} else if( month == 4 || month == 6 || month == 9 || month == 11) {
+//			days = 30;
+//		} else if( month == 2) {
+//			days = 28;
+//		} else {
+//			System.out.println("1~12월 까지만 넣어 주세요.");
+//			return;
+//		}
+		
+	switch(month) {
+		case 1: 
+		case 3: 
+		case 5:
+		case 7:
+		case 8: 
+		case 10:
+		case 12: {
 			days = 31;
-		} else if( month == 4 || month == 6 || month == 9 || month == 11) {
-			days = 30;
-		} else if( month == 2) {
+			break;
+		}
+		case 2: {
 			days = 28;
-		} else {
+			break;
+		}
+		case 4:
+		case 6:
+		case 9:
+		case 11: {
+			days = 30;
+			break;
+		}
+		default: {
 			System.out.println("1~12월 까지만 넣어 주세요.");
 			return;
 		}
-		
+	}
 		
 		System.out.println(month + "월은 " + days + "일 까지 있습니다.");
 	}
